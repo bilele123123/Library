07dbd6f77ee85cfeec5101108abba7a36c1bf2ed
bilele123123 <tple06203@gmail.com>
2023-06-19 22:13:47 -0700

    Add library catalog functionality; add new book, change status, remove book.

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-const libraryContent = document.querySelector(".library-content");
-const addBook = document.getElementById("add-book");
+const bookList = document.querySelector(".book-list");
+const addBookForm = document.getElementById("add-book-form");
 const addBookBTN = document.querySelector(".content-addBTN");
-const bookPopup = document.querySelector(".bookPopup");
+const bookPopup = document.querySelector(".book-popup");
 
-addBook.addEventListener("submit", addBookToLibrary);
+addBookForm.addEventListener("submit", addBookToLibrary);
 
 addBookBTN.addEventListener("click", () => {
     bookPopup.style.display = "block";
@@ -13,65 +13,80 @@ bookPopup.addEventListener("click", (event) => {
     if (event.target === bookPopup) {
         bookPopup.style.display = "none";
     }
-})
+});
 
 let myLibrary = [];
 
-function Book() {
-
+function Book(title, author, pages, hasRead) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.hasRead = hasRead;
 }
 
-function addBookToLibrary() {
+function addBookToLibrary(event) {
     event.preventDefault();
 
-    const title = prompt("Title:");
-    const author = prompt("Author:");
-    const pages = prompt("Pages:");
-    const hasRead = confirm("Completed Reading?");
+    const titleInput = document.getElementById("titleInput");
+    const authorInput = document.getElementById("authorInput");
+    const pageInput = document.getElementById("pageInput");
+    const readInput = document.getElementById("readInput");
+
+    const title = titleInput.value;
+    const author = authorInput.value;
+    const pages = pageInput.value;
+    const hasRead = readInput.checked;
 
     const newBook = new Book(title, author, pages, hasRead);
     myLibrary.push(newBook);
 
-    document.getElementById("add-book").reset();
+    titleInput.value = "";
+    authorInput.value = "";
+    pageInput.value = "";
+    readInput.checked = false;
+
+    bookPopup.style.display = "none";
+
     displayBook();
 }
 
 function displayBook() {
-    libraryContent.innerHTML = "";
+    bookList.innerHTML = "";
 
     myLibrary.forEach((book, index) => {
         const bookInfo = document.createElement("div");
         bookInfo.classList.add("book-info");
 
-        const bookTitle = document.createElement("h2");
+        const bookTitle = document.createElement("p");
+        bookTitle.classList.add("book-title");
         bookTitle.textContent = book.title;
 
         const bookAuthor = document.createElement("p");
+        bookAuthor.classList.add("book-author")
         bookAuthor.textContent = book.author;
 
         const bookPages = document.createElement("p");
+        bookAuthor.classList.add("book-pages")
         bookPages.textContent = book.pages;
 
-        const bookStatus = document.createElement("p");
-        bookStatus.textContent = `${book.hasRead ? "Completed" : "Incomplete"}`
-
+        const statusBTN = document.createElement("button");
+        statusBTN.textContent = book.hasRead ? "Complete" : "Incomplete";
+        statusBTN.classList.add("status-button");
+        statusBTN.classList.add(book.hasRead ? "complete" : "incomplete");
+        statusBTN.addEventListener("click", () => toggleReadStatus(index));
+            
         const removeBTN = document.createElement("button");
         removeBTN.textContent = "Remove";
+        removeBTN.classList.add("remove-bookBTN");
         removeBTN.addEventListener("click", () => removeBook(index));
-
-        const statusBTN = document.createElement("button");
-        statusBTN.textContent = "Have you read it?";
-        statusBTN.addEventListener("click", () => toggleReadStatus(index));
 
         bookInfo.appendChild(bookTitle);
         bookInfo.appendChild(bookAuthor);
         bookInfo.appendChild(bookPages);
-        bookInfo.appendChild(bookStatus);
-        bookInfo.appendChild(removeBTN);
         bookInfo.appendChild(statusBTN);
-    
-        libraryContent.appendChild(bookInfo);
-    })
+        bookInfo.appendChild(removeBTN);
+        bookList.appendChild(bookInfo);
+    });
 }
 
 function removeBook(index) {
@@ -82,4 +97,8 @@ function removeBook(index) {
 function toggleReadStatus(index) {
     myLibrary[index].hasRead = !myLibrary[index].hasRead;
     displayBook();
-}
+    const statusBTN = document.querySelectorAll(".status-button");
+    statusBTN[index].textContent = myLibrary[index].hasRead ? "Complete" : "Incomplete";
+    statusBTN[index].style.backgroundColor = myLibrary[index].hasRead ? "lightgreen" : "lightcoral";
+  }
+  
